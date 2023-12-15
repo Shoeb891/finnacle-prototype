a73a40f43a1a77d8ce23bf5f364207431cc55cac
Shoeb Mohiuddin <shoebm891@gmail.com>
2023-12-15 12:16:56 +0530

    ...

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,27 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-//   const path = request.nextUrl.pathname;
+  const path = request.nextUrl.pathname;
 
-//   const publicPaths = ['/', '/signin', '/signup'];
+  const publicPaths = ['/', '/signin', '/signup'];
 
-//   const isPublicPath = publicPaths.includes(path);
+  const isPublicPath = publicPaths.includes(path);
 
-//   const token = request.cookies.get('token')?.value || '';
+  const token = request.cookies.get('token')?.value || '';
 
-//   console.log('Path:', path);
-//   console.log('Is Public Path:', isPublicPath);
+  console.log('Path:', path);
+  console.log('Is Public Path:', isPublicPath);
 
-//   if (token && isPublicPath) {
-//       return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
+  if (token && isPublicPath) {
+      return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
     
-//   }
+  }
 
-//   if (!token && !isPublicPath) {
-//     return NextResponse.redirect(new URL('/', request.nextUrl));
-//   }
+  if (!token && !isPublicPath) {
+    return NextResponse.redirect(new URL('/', request.nextUrl));
+  }
 
-//   return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
