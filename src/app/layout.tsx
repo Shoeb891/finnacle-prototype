f0f257178497bb9507ecaa0577d366c49d8e83b6
Shoeb Mohiuddin <shoebm891@gmail.com>
2023-12-17 02:24:58 +0530

    Charts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <Sidebar/>
+        {/* <Navbar />
+        <Sidebar/> */}
         {children}
       </body>
     </html>
--- src/context/CryptoContext.tsx
+++ src/context/CryptoContext.tsx
@@ -43,7 +43,7 @@ interface CryptoData {
 
   const getCryptoData = async () => {
     try {
-      const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`)
+      const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage='1h%2C24h%2C7d'&locale=en`)
         .then(res => res.json());
         console.log(data)
       setCryptoData(data);
